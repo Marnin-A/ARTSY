1c7b2924ea843a08b7b1e701bcc380c1819cd218
Marnin Audu <msmaudu@gmail.com>
2022-11-29 00:05:45 +0100

    Created component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function App(props) {
   return (
     <div className="App">
       <h1>Every hit Movie in one place🎥</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
     </div>
   );
 }
