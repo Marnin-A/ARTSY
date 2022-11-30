a8522f2157d72675631572c914ea6ed24dd3b6c1
Marnin Audu <msmaudu@gmail.com>
2022-11-30 01:14:47 +0100

    Reimplemented components and deleted CRA boilerplate

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const header = (props) => {
+  return (
+    <header className="App-Header">
+      <h2>{props.text}</h2>
+    </header>
+  );
+};
+export default header;
