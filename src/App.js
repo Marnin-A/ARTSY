3fed654f94580fdd652a220adc9896545fb75ec8
Marnin Audu <msmaudu@gmail.com>
2022-12-02 03:24:49 +0100

    Project Development Paused

--- src/App.js
+++ src/App.js
@@ -77,9 +77,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <Header text="HOOKED" />
+      <Header text="Thrillers" />
       <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <p className="App-intro">All the most Thrilling movies in one place</p>
       <div className="movies">
         {loading && !errorMessage ? (
           <span>loading... </span>
