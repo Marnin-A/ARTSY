51173d0b1f4667c36c172c56c782ece6b16ce7b3
Marnin Audu <msmaudu@gmail.com>
2023-01-02 19:53:19 +0100

    Changed font links, and addded basic home page styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import { Navbar } from "./components";
-import { Auction, Home, Details, Product } from './pages'
+import { Auction, Home, Details, Product } from "./pages";
 // now isnt this easier :D
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       <Auction />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./home.module.css";
 
- export const Home = () => {
-  return <div>home</div>;
+export const Home = () => {
+  return (
+    <div className={styles.home}>
+      <section className={styles.landing}>
+        <h1 className={styles.landing__header}>
+          Photography is poetry & beautiful untold stories
+        </h1>
+        <p className={styles.landing__text}>
+          Flip through more than 10,000 vintage shots, old photograghs, historic
+          images and captures seamlessly in one place. Register to get top
+          access.
+        </p>
+      </section>
+    </div>
+  );
 };
-
-
