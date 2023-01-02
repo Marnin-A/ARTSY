b5912ae3a6a05339c6cca9a469187f8dc0f7ded5
Marnin Audu <msmaudu@gmail.com>
2023-01-02 11:50:29 +0100

    Added boilerplate for components, imported fonts, and imported pages to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import navbar from "./components/navbar/navbar";
+import home from "./pages/home/home";
+import details from "./pages/details/details";
+import product from "./pages/products/product";
 
 function App() {
   return <div className="App"></div>;
