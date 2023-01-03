0721f35f1fca1545d7c0e59d6a32c7c68fae787e
Marnin Audu <msmaudu@gmail.com>
2023-01-03 01:10:22 +0100

    Added featured products section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,6 @@ export const Home = () => {
     <div className={styles.home}>
       {/* Intro section*/}
       <section className={styles.intro}>
-        <img src="" alt="" />
         <h1 className={styles.intro__header}>
           Photography is poetry & beautiful untold stories
         </h1>
@@ -26,8 +25,9 @@ export const Home = () => {
 
       {/* Products section */}
       <section className={styles.products}>
-        <h2 className={styles.products__header}>Featured products</h2>
+        <h3 className={styles.products__header}>Featured products</h3>
         <hr />
+        {/* First product */}
         <div className={styles.product}>
           <img className={styles.product__img} src="" alt="" />
           <div className={styles.product__details}>
@@ -37,16 +37,87 @@ export const Home = () => {
               aliquam, purus sit amet luctus venenatis, lectus magna fringilla
               urna, porttitor rhoncus dolor pur
             </p>
-            <div className={styles.product__creators}>
-              <img className={styles.product__creator} src="" alt="" />
-              <img className={styles.product__creator} src="" alt="" />
-              <img className={styles.product__creator} src="" alt="" />
-              <img className={styles.product__creator} src="" alt="" />
-              <img className={styles.product__creator} src="" alt="" />
+            <div className={styles.creators}>
+              <ul className={styles.creators__imgs}>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+              </ul>
+              <span id="num_of_creators">64 major creators</span>
+              <button className={styles.details__btn}></button>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        {/* Second product */}
+        <div className={styles.product}>
+          <div className={styles.product__details}>
+            <h3>The Boolean Egyptian</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor pur
+            </p>
+            <div className={styles.creators}>
+              <ul className={styles.creators__imgs}>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+              </ul>
+              <span id="num_of_creators">64 major creators</span>
+              <button className={styles.details__btn}></button>
+            </div>
+          </div>
+          <img className={styles.product__img} src="" alt="" />
+        </div>
+
+        <hr />
+
+        {/* Third product */}
+        <div className={styles.product}>
+          <img className={styles.product__img} src="" alt="" />
+          <div className={styles.product__details}>
+            <h3>The Boolean Egyptian</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor pur
+            </p>
+            <div className={styles.creators}>
+              <ul className={styles.creators__imgs}>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+                <li className={styles.creator}></li>
+              </ul>
+              <span id="num_of_creators">64 major creators</span>
+              <button className={styles.details__btn}></button>
             </div>
           </div>
         </div>
       </section>
+
+      {/* Upcoming Section */}
+      <section className={styles.upcoming}>
+        <div className={styles.Uheader__container}>
+          <h4 className={styles.upcoming__header}>
+            See Upcoming Auctions and Exhibitions
+          </h4>
+          <hr className={styles.arrow3} />
+        </div>
+      </section>
+      {/* Explore Section */}
+      <section className={styles.explore}></section>
+      {/* Top Creators Section */}
+      <section className={styles.top__creators}></section>
+      <footer></footer>
     </div>
   );
 };
