b4033a8c155d225b48d9454b506d6bc95ca70f08
Marnin Audu <msmaudu@gmail.com>
2023-01-19 20:02:26 +0100

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
 import { Navbar } from "./components";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Auction, Home, Details, Product } from "./pages";
 // now isnt this easier :D
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "auction", element: <Auction /> },
+  { path: "details", element: <Details /> },
+  { path: "products", element: <Product /> },
+]);
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      <Auction />
+      <RouterProvider router={router} />
     </div>
   );
 }
