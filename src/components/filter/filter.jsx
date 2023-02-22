3768ffb80c04e76d5c5c19d41894e170f885e1be
Marnin Audu <msmaudu@gmail.com>
2023-02-22 21:45:49 +0100

    Finish filter UI

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -3,25 +3,40 @@ import styles from "./filter.module.css";
 import Search from "../../assets/search.svg";
 import Sliders from "../../assets/Sliders.svg";
 import UpToggle from "../../assets/Uptoggle.svg";
-import DownToggle from "../../assets/DownToggle.svg";
+import MultiRangeSlider from "../multiRangeSlider/MultiRangeSlider";
 
 const Filter = () => {
-  const [toggleUp, setToggle] = useState();
+  const [toggleUp, setToggle] = useState(1);
+  const [toggleUp1, setToggle1] = useState(1);
+  const [toggleUp2, setToggle2] = useState(1);
+  const [toggleUp3, setToggle3] = useState(1);
 
   const style = {
     transform: toggleUp ? "rotate(180deg)" : "",
     transition: "transform 150ms ease", // smooth transition
   };
+  const style1 = {
+    transform: toggleUp1 ? "rotate(180deg)" : "",
+    transition: "transform 150ms ease", // smooth transition
+  };
+  const style2 = {
+    transform: toggleUp2 ? "rotate(180deg)" : "",
+    transition: "transform 150ms ease", // smooth transition
+  };
+  const style3 = {
+    transform: toggleUp3 ? "rotate(180deg)" : "",
+    transition: "transform 150ms ease", // smooth transition
+  };
   let count = 0;
+  const Toggler = () => {
+    count++;
+    count % 2 == 0 ? setToggle(false) : setToggle(true);
+  };
   return (
     <div className={styles.filter}>
       <div className={styles.search_bar}>
         <span>
-          <img
-            style={{ width: "1em", marginLeft: "1em", marginRight: "1em" }}
-            src={Search}
-            alt="search icon"
-          />
+          <img id={styles.search_icon} src={Search} alt="search icon" />
         </span>
         <input id={styles.search} type="text" placeholder="Search" />
       </div>
@@ -47,22 +62,126 @@ const Filter = () => {
           borderRadius: "0.375em",
         }}
       />
-      <div className={styles.categories}>
-        <h3>By Category</h3>
-        <button
-          onClick={() => {
-            count++;
-            count % 2 == 0 ? setToggle(false) : setToggle(true);
-          }}
-          style={{
-            width: "min-content",
-            background: "none",
-            outline: "none",
-            border: "none",
-          }}
-        >
-          <img style={style} src={UpToggle} alt="" />
-        </button>
+      <div
+        style={{
+          marginTop: "5%",
+          marginBottom: "5%",
+        }}
+      >
+        <div className={styles.filter_section}>
+          <h3>By Category</h3>
+          <button
+            onClick={() => {
+              count++;
+              count % 2 == 0 ? setToggle(false) : setToggle(true);
+            }}
+            className={styles.toggle}
+          >
+            <img style={style} src={UpToggle} alt="" />
+          </button>
+        </div>
+        <div>
+          <label htmlFor="editorial" className={styles.checkLabel}>
+            <input
+              type="checkbox"
+              name="editorial"
+              id="editorial"
+              className={styles.checkBox}
+            />
+            Editorial
+          </label>
+          <label htmlFor="fashion" className={styles.checkLabel}>
+            <input type="checkbox" name="fashion" className={styles.checkBox} />
+            Fashion
+          </label>
+          <label htmlFor="optics" className={styles.checkLabel}>
+            <input type="checkbox" name="optics" className={styles.checkBox} />
+            Optics
+          </label>
+          <label htmlFor="art&Museum" className={styles.checkLabel}>
+            <input
+              type="checkbox"
+              name="art&Museum"
+              className={styles.checkBox}
+            />
+            Art & Museum
+          </label>
+          <label htmlFor="nature" className={styles.checkLabel}>
+            <input type="checkbox" name="nature" className={styles.checkBox} />
+            Nature
+          </label>
+        </div>
+      </div>
+      <div
+        style={{
+          marginBottom: "5%",
+        }}
+      >
+        <div className={styles.filter_section}>
+          <h3>By Price</h3>
+          <button
+            onClick={() => {
+              count++;
+              count % 2 == 0 ? setToggle1(false) : setToggle1(true);
+            }}
+            className={styles.toggle}
+          >
+            <img style={style1} src={UpToggle} alt="" />
+          </button>
+        </div>
+        <div>
+          <MultiRangeSlider
+            style={{ position: "relative" }}
+            min={0}
+            max={20000}
+            onChange={({ min, max }) =>
+              console.log(`min = ${min}, max = ${max}`)
+            }
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          marginBottom: "5%",
+        }}
+      >
+        <div className={styles.filter_section}>
+          <h3>By Artist</h3>
+          <button
+            onClick={() => {
+              count++;
+              count % 2 == 0 ? setToggle2(false) : setToggle2(true);
+            }}
+            className={styles.toggle}
+          >
+            <img style={style2} src={UpToggle} alt="" />
+          </button>
+        </div>
+        <div className={styles.costs}>
+          <button className={styles.cost}>All</button>
+          <button className={styles.cost}>Below $100.00</button>
+          <button className={styles.cost}>$100.00 - $150.00</button>
+          <button className={styles.cost}>$150.00 - $200.00</button>
+          <button className={styles.cost}>Above $200.00</button>
+        </div>
+      </div>
+      <div
+        style={{
+          marginBottom: "5%",
+        }}
+      >
+        <div className={styles.filter_section}>
+          <h3> Collection year</h3>
+          <button
+            onClick={() => {
+              count++;
+              count % 2 == 0 ? setToggle3(false) : setToggle3(true);
+            }}
+            className={styles.toggle}
+          >
+            <img style={style3} src={UpToggle} alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
